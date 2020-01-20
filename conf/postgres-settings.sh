e31f2eba7573292dcbbcaa13bd41b0a8a8f5f802
audour <xhemp@mykolab.com>
2020-01-20 17:00:50 +0100

    Add default disabled pgsql logging settings

--- conf/postgres-settings.sh
+++ conf/postgres-settings.sh
@@ -24,3 +24,5 @@ sed -i -e"s/^#random_page_cost = 4.0.*$/random_page_cost = 2.0/" ${PGDATA}/postg
 sed -i -e"s/^#cpu_tuple_cost = 0.01.*$/cpu_tuple_cost = 0.05/" ${PGDATA}/postgresql.conf
 sed -i -e"s/^#autovacuum_vacuum_scale_factor = 0.2.*$/autovacuum_analyze_scale_factor = 0.2/" ${PGDATA}/postgresql.conf
 #sed -i -e"s/^#full_page_writes = on.*$/full_page_writes = off/" ${PGDATA}/postgresql.conf
+#sed -i -e"s/^#logging_collector = off.*$/logging_collector = on/" ${PGDATA}/postgresql.conf
+#sed -i -e"s/^#log_statement = 'none'.*$/log_statement = 'all'/" ${PGDATA}/postgresql.conf
